b157574088e042c50129c36fe80cd6a1a6648aa9
kimgun95 <harrison1995@naver.com>
2021-04-22 22:08:22 +0900

    add if loop

--- april_week04/10250_acm_hotel.py
+++ april_week04/10250_acm_hotel.py
@@ -1,7 +1,10 @@
 def acm_hotel(num):
     for i in range(num):
         H, W, N = map(int, input().split())
-        result = N % H * 100 + N // H + 1
+        if N % H == 0:
+            result = H * 100 + N//H
+        else:
+            result = N % H * 100 + N // H + 1
         print(result)
 
 
