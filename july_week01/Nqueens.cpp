3878e8d0c7a83403af20acfbb8512772f0f363cd
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-07-06 21:48:05 +0900

    solve SSD algorithm day2

--- july_week01/Nqueens.cpp
+++ july_week01/Nqueens.cpp
@@ -1,45 +1,93 @@
-//n = int(input())
-//# n크기 만큼 배열 초기화
-//q = [0 for i in range(n)]
-//cnt = 0
-//
-//# 가지치기 함수(유망하지 않은 노드들은 탐색하지 않으며 유망한 노드에 대해서만 탐색을 한다)
-//def pruning(q, n):
-//    # 이전에 놓았던 퀸들과 동선이 겹치는지 확인
-//    for i in range(n):
-//        # 같은 열에 있다면 false
-//        if q[i] == q[n]:
-//            return 0
-//        #  오른쪽 위 방향에 퀸이 있다면 false
-//        if (q[i] - q[n]) == (n - i):
-//            return 0
-//        #  왼쪽 위 방향에 퀸이 있다면 false
-//        if (q[n] - q[i]) == (n - i):
-//            return 0
-//    return 1
-//
-//# 0번째 배열부터 퀸을 하나씩 추가한다
-//# n은 0이 첫 시작이고 n + 1을 통해 늘려간다
-//def nQueen(q, n):
-//    length = len(q)
-//    # 퀸을 끝까지 채우는데 성공하면 cnt를 증가시킨다
-//    if n == length:
-//        global cnt
-//        cnt += 1
-//    else:
-//        # n번째 배열에 i위치에 퀸을 놓는다
-//        for i in range(length):
-//            q[n] = i
-//            # 가지치기를 실시하며 유망한 노드 발견시 다음 배열에 퀸 배치를 시작한다
-//            if pruning(q, n) == 1:
-//                nQueen(q, n+1)
-//
-//nQueen(q, 0)
-//print(cnt)
-
+// 9663 - Nqueen
+// 진도상 따라 잡아야 하기에 코드를 보며 진행했음.
+// 예전에 내가 푼 python코드를 보고 c++로 옮겨보려 했으나 도저히 코드가 이해가 안가 포기. 
+// 강사님 코드랑 초반부 진행은 비슷했고 이후 진행은 강사님 코드가 이해하기 쉬워 그대로 사용.
+// 완전탐색? 백트래킹? 을 사용한 것이고 탐색하던 곳을 재귀로 계속 파고 들다가 탐색이 끝나면
+// visited를 다시 원래대로 재설정해야 하는 것은 항상 중요...! 
 #include <iostream>
 using namespace std;
 
-int main() {
-	return 0;
+int n, ans;
+int chess[14][14];  // 초기화가 이미 0으로 되어있음  
+
+// line : 0 ~ n - 1
+void recur(int line) {
+    // 종료조건. line 값이 n이 되는 것만 끝까지 queen을 채운 것이고 ans++ 및 return 
+    if (line == n) {
+        ans++;
+        return;
+    }
+    // line어딘가에다가 queen을 놓아본다
+    for (int i = 0 ; i < n ; i++) {
+    	// 놓으려는 곳이 -1로 초기화 되어있지 않다면 놓지 못하기 때문에 continue 
+        if (chess[line][i] != -1) continue;
+        
+        chess[line][i] = line;
+        // line에 놓았으니까 상/하/좌/우/대각선에는 queen을 놓지 못하게 처리한다 
+        // 좌/우 
+        for (int x = 0 ; x < n ; x++) {
+            if (chess[line][x] == -1) {
+                chess[line][x] = line;
+            }
+        }
+        // 하 (상이 제외인 이유는 이미 좌우 조건으로 윗줄에서 다른 숫자로 채워져있을 것이기 때문에) 
+        for (int y = line ; y < n ; y++) {
+            if (chess[y][i] == -1) {
+                chess[y][i] = line;
+            }
+        }       
+        // 대각선. 좌하로 가는 경로. 
+        for (int y = line, x = i ; y < n && 0 <= x ; y++, x--) {
+            if (chess[y][x] == -1) {
+                chess[y][x] = line;
+            }
+        } // 대각선. 우하로 가는 경로. 
+        for (int y = line, x = i ; y < n && x < n ; y++, x++) {
+            if (chess[y][x] == -1) {
+                chess[y][x] = line;
+            }
+        }
+
+        // 다음 줄 queen을 놓아본다
+        recur(line + 1);
+
+		// 위에서 탐색을 모두 끝내고 오는 길이니 
+        // queen이 처리한 흔적을 지운다. line 값을 다시 -1로. 
+        for (int x = 0 ; x < n ; x++) {
+            if (chess[line][x] == line) {
+                chess[line][x] = -1;
+            }
+        }
+        // 하
+        for (int y = line ; y < n ; y++) {
+            if (chess[y][i] == line) {
+                chess[y][i] = -1;
+            }
+        }       
+        // 대각선
+        for (int y = line, x = i ; y < n && 0 <= x ; y++, x--) {
+            if (chess[y][x] == line) {
+                chess[y][x] = -1;
+            }
+        }
+        for (int y = line, x = i ; y < n && x < n ; y++, x++) {
+            if (chess[y][x] == line) {
+                chess[y][x] = -1;
+            }
+        }
+    }
 }
+
+int main() {
+    // array fill
+    for (int i = 0 ; i < 14 ; i++) {
+        for (int j = 0 ; j < 14 ; j++) {
+            chess[i][j] = -1;
+        }
+    }
+    cin >> n;
+    // todo
+    recur(0);
+    cout << ans;
+}
+
---
+++ july_week01/스도쿠.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int arr[9][9];
+
+int main() {
+	// 스도쿠 입력 받기. 
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	
+} 
