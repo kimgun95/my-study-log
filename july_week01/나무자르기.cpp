807d37aa4aed126feb618a4becfff0c37c74091e
kimgun95 <harrison1995@naver.com>
2021-07-08 00:46:05 +0900

    using binary search and 자료형 크기 유의

--- july_week01/나무자르기.cpp
+++ july_week01/나무자르기.cpp
@@ -1,20 +1,36 @@
+// 2805 - 나무자르기 
+// 단순히 모든 나무를 모든 길이로 자르려고 했다가  시간초과가 발생했다.
+// 이분탐색을 해야 했고 마지막 제출 때 틀렸다는 답변을 받았는데. 이유는 자료형 때문이다.
+// 조건에서 나무 길이가 20억 이하로 조건이 있어서 int로 해결이 되겠지 했는데.
+// 아래 수식에서 mid계산이나 cnt계산 결과로 int 자료형 크기를 넘는 값을 받을 수 있어서 long로 바꿔주었다. 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int N, M;
-int tree[1000000];
-int high = 0;
-int ans;
+long N, M;
+long tree[1000000];
+long highest = 0;
+long ans = 0;
+long mid;
 
-int cut() {
-	for (int i = high - 1; i >= 0; i--) {
-		int len = 0;
-		for (int j = 0; j < N; j++) {
-			if (tree[j] > i) len += tree[j] - i;
-		}
-		if (len >= M) return i;
+void cut(long low, long high) {
+	mid = (low + high) / 2;
+	long cnt = 0;
+	
+	if (low > high) return;
+	
+	for (int i = N - 1; i >= 0; i--) {
+		if (tree[i] - mid < 0) break;
+		cnt += tree[i] - mid;
 	}
+	
+	if (cnt >= M) ans = max(ans, mid);
+	
+	if (cnt < M) cut(low, mid - 1);
+	else if (cnt > M) cut(mid + 1, high);
+	return;
 }
+	
 
 
 int main() {
@@ -22,9 +38,11 @@ int main() {
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		cin >> tree[i];
-		if (tree[i] > high) high = tree[i];
+		if (tree[i] > highest) highest = tree[i];
 	}
-	ans = cut();
+	sort(tree, tree + N);
+	
+	cut(0, highest);
 	cout << ans;
 	return 0;
 } 
