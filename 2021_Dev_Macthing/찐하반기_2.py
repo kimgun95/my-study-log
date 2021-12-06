1fe76ca1eff36e5bb542c0de0da20013b2b19086
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-12-06 17:33:42 +0900

    update comment

--- 2021_Dev_Macthing/찐하반기_2.py
+++ 2021_Dev_Macthing/찐하반기_2.py
@@ -3,31 +3,34 @@ from collections import deque
 
 def solution(n, recipes, orders):
     r, o = {}, deque()
+    # 요리의 조리 시간은 딕셔너리로 저장
     for i in range(len(recipes)):
         key, val = recipes[i].split()
         r[key] = int(val)
+    # 주문되는 요리는 순서대로 리스트에 저장
     for i in range(len(orders)):
         lst = orders[i].split()
         o.append([lst[0], int(lst[1])])
 
     time = 0
     count = 0 # 마지막 음식인지 판단
-    waiting = deque() # 음식 순서대로 저장
+    waiting = deque() # 시간 때 마다 주문되는 음식 순서대로 저장
     fire = [0] * n # 화구
-    # 1. 대기 음식 저장
-    # 2. 요리중인 음식 -1
-    # 3. 빈 화구에 조리 시작
-    # 대기 음식도 없고 오더도 모두 빠졌을 때 종료
+
     while True:
         time += 1
+        # 1. 대기 음식 추가
         if o and o[0][1] == time:
             waiting.append(o.popleft())
         for i in range(n):
+            # 2. 요리중인 음식 -1초
             if fire[i] != 0:
                 fire[i] -= 1
+            # 3. 빈 화구에 조리 시작
             if fire[i] == 0 and waiting:
                 fire[i] = r[waiting[0][0]]
                 count += 1
+                # 4. 마지막 음식이라면 리턴
                 if count == len(orders):
                     return time + fire[i]
                 waiting.popleft()
