d45cf06c1a354950f0c077fb5ab8c9f759aac2a2
kimgun95 <harrison1995@naver.com>
2021-12-09 18:06:31 +0900

    hard to solve: using DP

---
+++ LeetCode/Trees/SymmetricTree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 스택에 자식 쌍을 하나의 규칙으로 넣어주는 것 -> 이 문제의 근본 해결책이 된다(dynamic programming)
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        stack = [(root.left, root.right)]
+        while stack:
+            left, right = stack.pop()
+            # 둘에게 자식이 모두 없다면 다음 스택 진행
+            if left is None and right is None:
+                continue
+            # 한쪽만 자식이 없다면 균형이 깨진 것이므로 false 반환
+            if left is None or right is None:
+                return False
+            # 본격적인 비교, 같지 않다면 대칭이 아닌 것
+            # 같다면 자식추가, 이때 스택에 아래와 같은 쌍으로 넣어야 함 -> dp 개념
+            if left.val == right.val:
+                stack.append((left.left, right.right))
+                stack.append((left.right, right.left))
+            else:
+                return False
+        # 모든 비교가 잘 끝났다면 true 반환
+        return True
