b6224ada7ebe9a52621ea8185fb4768f0bef3f16
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-12-13 18:20:16 +0900

    add another solution

--- LeetCode/Others/HammingDistance.py
+++ LeetCode/Others/HammingDistance.py
@@ -2,4 +2,12 @@
 # 위 정의를 착안하여 x와 y가 서로 다른 지점이 몇 개인지 파악하는 게 중요 -> xor 연산 적합
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        return bin(x ^ y).count('1')
+        # 1. 내 풀이
+        # return bin(x ^ y).count('1')
+        # 2. 함수 사용 없는 풀이
+        xor = x ^ y
+        count = 0
+        for _ in range(32):
+            count += xor & 1
+            xor = xor >> 1
+        return count
