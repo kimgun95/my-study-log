999f2c2450adb9b012b53b5b2bf73665b21b8fbb
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-12-16 20:52:31 +0900

    hard: using stack

---
+++ LeetCode/BinaryTree/SolveTreeProblemsRecursively/SymmetricTree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 스택에 자식 쌍을 하나의 규칙으로 넣어주는 것 -> 이 문제의 근본 해결책이 된다(dynamic programming)
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        stack = [(root.left, root.right)]
+        while stack:
+            left, right = stack.pop()
+            # 두 값이 모두 null이면 비교할 것이 없으니 다음 스택 진행
+            if left is None and right is None:
+                continue
+            # 하나만 null이면 false 반환
+            elif left is None or right is None:
+                return False
+            # 둘 다 값일 때 비교
+            # 같다면 스택 추가, 이때 스택에 아래와 같은 쌍으로 넣어야 함 -> dp 개념
+            else:
+                if left.val == right.val:
+                    stack.append((left.left, right.right))
+                    stack.append((left.right, right.left))
+                else:
+                    return False
+        # 모든 비교가 잘 끝났다면 true 반환
+        return True
