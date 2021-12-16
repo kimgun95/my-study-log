9216978e21e97eb5c98f1f3eff766260b3f60d50
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-12-16 19:37:18 +0900

    update comment

--- LeetCode/Trees/SymmetricTree.py
+++ LeetCode/Trees/SymmetricTree.py
@@ -13,14 +13,14 @@ class Solution:
         stack = [(root.left, root.right)]
         while stack:
             left, right = stack.pop()
-            # 둘에게 자식이 모두 없다면 다음 스택 진행
+            # 두 값이 모두 null이면 비교할 것이 없으니 다음 스택 진행
             if left is None and right is None:
                 continue
-            # 한쪽만 자식이 없다면 균형이 깨진 것이므로 false 반환
+            # 하나만 null이면 false 반환
             if left is None or right is None:
                 return False
-            # 본격적인 비교, 같지 않다면 대칭이 아닌 것
-            # 같다면 자식추가, 이때 스택에 아래와 같은 쌍으로 넣어야 함 -> dp 개념
+            # 둘 다 값일 때 비교
+            # 같다면 스택 추가, 이때 스택에 아래와 같은 쌍으로 넣어야 함 -> dp 개념
             if left.val == right.val:
                 stack.append((left.left, right.right))
                 stack.append((left.right, right.left))
