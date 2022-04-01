1b093196319cef645965e9aa52468d58cc4b58ab
kimgun95 <harrison1995@naver.com>
2022-04-01 18:01:33 +0900

    hard:O(n)으로 생각하기

--- 2022/April/lv4_지형편집.py
+++ 2022/April/lv4_지형편집.py
@@ -1,7 +1,19 @@
 def solution(land, P, Q): # 추가P, 제거Q
-    answer = 0
+    land2 = []
+    for l in land:
+        land2.extend(l)
+    land2.sort()
+    N = len(land2)
+    cost = (sum(land2) - land2[0] * N) * Q # 최하층 생성
+    answer = cost
 
-
+    for i in range(1, N):
+        if land2[i] != land2[i - 1]: # 새로운 층을 생성해야 한다면
+            dif = land2[i] - land2[i - 1]
+            cost += (dif * i * P) - (dif * (N - i) * Q)
+            if answer < cost:
+                break
+            answer = cost
     return answer
 
 
