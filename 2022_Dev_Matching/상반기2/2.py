659e017f4ebf850102b3b0e3d290474e6e1e678a
FBI00851 <rlarjs7879@pionnet.co.kr>
2022-07-05 17:17:36 +0900

    시험때 바보같이 하드코딩은 하지말자 이렇게 할 수 있자나

--- 2022_Dev_Matching/상반기2/2.py
+++ 2022_Dev_Matching/상반기2/2.py
@@ -1,70 +1,41 @@
 def solution(n, horizontal):
     answer = [[0 for _ in range(n)] for _ in range(n)]
     idx = 1
-    dir = [[0,1], [0,-1], [1,0], [-1,0]] # 우, 좌, 하, 상
-    di = 0
-    if horizontal == True:
+    dir = [[0,1], [1,0], [0,-1], [-1,0]] # 동남서북
+    if horizontal:
         di = 0
     else:
-        di = 2
+        di = 1
 
     def clean(y, x, size):
         nonlocal idx, di
-
-        answer[y][x] = idx
-        idx += 1
-
         ny, nx = y, x
-
-        while True:
-            if di == 0:
-                if nx == size:
-                    break
-            elif di == 1:
-                if nx == 0:
-                    break
-            elif di == 2:
-                if ny == size:
-                    break
-            elif di == 3:
-                if ny == 0:
-                    break
-            ny, nx = ny + dir[di][0], nx + dir[di][1]
-            answer[ny][nx] = idx
+        # 1. 직진할 수 있을 때 까지
+        while 0 <= y <= size and 0 <= x <= size:
+            answer[y][x] = idx
             idx += 1
-        if n % 2 != 0:
-            if ny == 0 and nx == n - 1:
-                print(ny, nx)
-                return
-        elif n % 2 == 0:
-            if ny == n - 1 and nx == 0:
-                return
-
-
-        if di == 0: # 우측 이동
-            if ny == 0: # down 이동
-                di = 2
-            else: # up
-                di = 3
-            clean(ny + dir[di][0], nx + dir[di][1], size)
-        elif di == 2: # 아래 이동
-            if nx == 0: # right 이동
-                di = 0
-            else: # left
-                di = 1
-
-            clean(ny + dir[di][0], nx + dir[di][1], size)
-        elif di == 1:
-            di = 2
-            clean(ny + dir[di][0], nx + dir[di][1], size + 1)
-        else:
-            di = 0
-            clean(ny + dir[di][0], nx + dir[di][1], size + 1)
-
+            ny, nx = y, x
+            y += dir[di][0]
+            x += dir[di][1]
+        # 2. size*size 크기 블럭을 채웠는지 확인
+        for i in range(1, 4):
+            d = (di + i) % 4
+            dy, dx = ny + dir[d][0], nx + dir[d][1]
+            if 0 <= dy <= size and 0 <= dx <= size and answer[dy][dx] == 0:
+                di = d
+                return clean(dy, dx, size)
+        # 3. size*size 블럭을 모두 채웠으면 다음 크기를 채우기
+        for i in range(1, 4):
+            d = (di + i) % 4
+            dy, dx = ny + dir[d][0], nx + dir[d][1]
+            if 0 <= dy < n and 0 <= dx < n and answer[dy][dx] == 0:
+                di = d
+                return clean(dy, dx, size + 1)
         return
 
     clean(0, 0, 1)
     return answer
 
-print(solution(4, True))
-print(solution(5, False))
+
+print(solution(4, True)) # 4*4 사이즈, 우측진행
+print(solution(5, False)) # 5*5 사이즈, 아래진행
